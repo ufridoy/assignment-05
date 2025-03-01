ed41c3a3343cc04db8105e2d1cbf5206db9e4e09
ufridoy <umorfarukridoy1@gmail.com>
2025-03-01 22:17:32 +0600

    task count add

--- js/script.js
+++ js/script.js
@@ -5,14 +5,29 @@ document.getElementById('theme-color-change').addEventListener('click',
         document.body.style.backgroundColor = randomColor;
     })
 
+    // current date setup
+const currentDate = new Date();
+const formateDate =currentDate.toLocaleDateString('en-US',{
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric', 
+})
+document.getElementById('current-date').innerText=formateDate;
+const currentWeekday = new Date();
+const formateWeekday =currentWeekday.toLocaleDateString('en-Us',{
+    weekday: 'short'
+})
+document.getElementById('current-weekday').innerText=formateWeekday;
+
 
 document.getElementById('first-complete-btn').addEventListener('click', 
     function (event) {
 
         const count = event.target.innerText === "+" ? -1 : 1; // 1 || -1
         const countIncrease = document.getElementById("nav-checkbox-count");
-    const countQuantity = parseInt(countIncrease.innerText);
-    const newQuantity = Math.max(0, countQuantity + count);
-    countIncrease.innerText = newQuantity;
-    
+        const countQuantity = parseInt(countIncrease.innerText);
+        const newQuantity = Math.max(0, countQuantity + count);
+        countIncrease.innerText = newQuantity;
+        alert('board alert successfully');
+
 })
